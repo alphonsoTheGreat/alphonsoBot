5b788498114851f233fae9ca1d09f15fb5d62214
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-03 19:16:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ const { Telegraf } = require('telegraf')
 const secrets = require('./secret.json')
 
 const bot = new Telegraf(secrets.token)
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('a', (ctx) => ctx.reply('hear'))
+// bot.hears('b', (ctx) => ctx.reply('b hear'))
+bot.command('b', (ctx) => ctx.reply('command'))
+
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+
+
+// bot.command("b")
 bot.launch()
+
+
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
