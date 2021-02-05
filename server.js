355a303c26af00915c8c7b1545b227cad058ee41
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-05 09:30:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const expressApp = express()
 const port = process.env.PORT || 5000 // Correct port will be returned here
 const bot = new Telegraf(process.env.BOT_TOKEN)
 console.log({port,token:process.env.BOT_TOKEN})
+
+bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
+
+
+
 expressApp.use(bot.webhookCallback('/itamir'))
 bot.telegram.setWebhook('https://alphonsobot.herokuapp.com/itamir')
 
