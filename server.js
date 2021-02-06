9d9030927f5939d7c0a94a3b4a37c8616a7a7cd5
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-06 17:45:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,23 @@
 const {Telegraf}  = require('telegraf')
 const express = require('express')
+const botService = require('./bot.js')
+
 const expressApp = express()
 
 const port = process.env.PORT || 5000 // Correct port will be returned here
-console.log({port,token:process.env.BOT_TOKEN})
+
+// init the bot
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-
-
-
+// tell telegram our webhook
 bot.telegram.setWebhook('https://alphonsobot.herokuapp.com/bot-hook')
 
-
-expressApp.use( (req, res,next) => {
-  console.log("---------------------body ------")
-  console.log(req.body)
-  console.log("---------------------query ------")
-  console.log(req.query)
-  console.log("---------------------params ------")
-  console.log(req.params)
-  console.log("---------------------headers ------")
-  console.log(req.headers)
-  next()
-})
+// set our webhook
 expressApp.use(bot.webhookCallback('/bot-hook'))
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('i still not sure what i do . . . '))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+botService.init()
 
-
-
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-expressApp.post('/bot-hook', (req, res,next) => {
-  console.log("****--------------------body ------")
-  console.log(req.body)
-  console.log("---------------------query ------")
-  console.log(req.query)
-  console.log("---------------------params ------")
-  console.log(req.params)
-  console.log("---------------------headers ------")
-  console.log(req.headers)
-  next()
-})
 
 expressApp.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`bot is listening on port ${port}!`)
 })
