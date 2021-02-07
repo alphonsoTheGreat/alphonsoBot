28d98e8775e4551764b32a3cfb84801a599d226d
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-07 11:00:43 +0200

    Create exampleBot.js

---
+++ exampleBot.js
@@ -0,0 +1,37 @@
+
+const Markup = require('telegraf/markup')
+const fetch = require('node-fetch')
+
+
+
+module.exports = {
+
+  runBot:(bot)=>{
+
+    bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+      const apiUrl = `http://recipepuppy.com/api/?q=${inlineQuery.query}`
+      const response = await fetch(apiUrl)
+      const { results } = await response.json()
+      const recipes = results
+        .filter(({ thumbnail }) => thumbnail)
+        .map(({ title, href, thumbnail }) => ({
+          type: 'article',
+          id: thumbnail,
+          title: title,
+          description: title,
+          thumb_url: thumbnail,
+          input_message_content: {
+            message_text: title
+          },
+          reply_markup: Markup.inlineKeyboard([
+            Markup.urlButton('Go to recipe', href)
+          ])
+        }))
+      return answerInlineQuery(recipes)
+    })
+
+      bot.on('chosen_inline_result', ({ chosenInlineResult }) => {
+      console.log('chosen inline result', chosenInlineResult)
+    })
+  }
+}
