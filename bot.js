3e0688a1dea7fc907b3c7c5e4818e3b5fbd5a55e
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-07 10:07:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
+const textMiddleware = (ctx) => ctx.reply('this is a text middleware'))
+
+
+
+
+
+
 module.exports = {
   
   runBot:(bot)=>{
-      
+    
+    bot.on('text', textMiddleware)
+    
     bot.start((ctx) => ctx.reply('Welcome'))
     bot.help((ctx) => ctx.reply('i still not sure what i do . . . '))
     bot.on('sticker', (ctx) => ctx.reply('👍'))
