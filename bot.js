2ffe13447f655c6983cb417e8de663d15baf80e5
alphonsoTheGreat <itay88tamir@gmail.com>
2021-02-07 10:29:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
-const textMiddleware = (ctx,next) => {
-  console.log({ctx})
-  console.log({obj:Object.keys(ctx)})
+
+const logReqInfo = (ctx,next) => {
+  console.log({
+    message:ctx.message,
+    userInfo:ctx.from,
+    type:ctx.updateType,
+    updateSubTypes:ctx.updateSubTypes
+  })
   next()
 }
-
-const responseWithTextMiddleware =(text)=> (ctx) =>   ctx.replay(text)
-
-
 
 
 
@@ -15,7 +16,8 @@ module.exports = {
   
   runBot:(bot)=>{
     
-    bot.on('text', textMiddleware,responseWithTextMiddleware("thank you for you kind words!"))
+    bot.use(logReqInfo)
+    bot.on('text',(ctx)=>ctx.replay(ctx.message.from.username) )
     
     bot.start((ctx) => ctx.reply('Welcome'))
     bot.help((ctx) => ctx.reply('i still not sure what i do . . . '))
