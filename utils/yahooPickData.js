009f7fa66c28657997dcb9d6c7bed2b6c4712d2d
itayt@foresightauto.com <itayt@foresightauto.com>
2021-02-12 15:14:55 +0200

    fix

--- utils/yahooPickData.js
+++ utils/yahooPickData.js
@@ -1,123 +1,127 @@
 module.exports = {
-    extractPE: function (response) {
-        if (response.summaryDetail.trailingPE)
-            return response.summaryDetail.trailingPE.raw
-        else return "cannot find"
-    },
-
-
-
-    extractMarketCap: function (response, formatted = true) {
-        if (response.summaryDetail.marketCap)
-            if (formatted)
-                return response.summaryDetail.marketCap.fmt
-            else return response.summaryDetail.marketCap.raw
-        else return "cannot find"
-    },
-
-
-    extractCurrentPrice: function (response) {
-        if (response.price.regularMarketPrice)
-            return response.price.regularMarketPrice.raw
-        else return "cannot find"
-    },
-
-
-    extractFreeCashFlow: function (response, formatted = true) {
-        if (response.timeSeries)
-            if (formatted)
-                return response.timeSeries.trailingFreeCashFlow[0].reportedValue.fmt
-            else
-                return response.timeSeries.trailingFreeCashFlow[0].reportedValue.raw
-        else return "cannot find"
-    },
-
-
-    extractEarnings: function (responseS) {
-        if (responseS === undefined) return
-
-        const earnings = responseS.earnings;
-        if (earnings) {
-            const calc = earnings.financialsChart.quarterly.reduce((prev, q) => {
-                return ({
-                    ...prev,
-                    [q.date]: {
-                        date: q.date,
-                        earnings: q.earnings.fmt.toString(),
-                        revenue: q.revenue.fmt.toString(),
-                        calc: ((q.earnings.raw / q.revenue.raw))
-                    }
-                })
-            }, {})
-
-            return calc
-        }
-        else return {}
-
-
-    },
-
-
-
     // ------------------------------------------------------------------------------------
     helpers: {
 
-        reduceEarningObject: function (earnings) {
-
-            const earningsKeys = Object.keys(earnings);
-
-            return earningsKeys.reduce((prev, current) => {
-
-                const Q_data = earnings[current];
-                prev.push({ key: "revenue_" + Q_data.date, value: Q_data.revenue })
-                prev.push({ key: "earnings_" + Q_data.date, value: Q_data.earnings })
-                prev.push({ key: "calc_" + Q_data.date, value: Q_data.calc })
-                return prev
-            }, [])
-        },
-
-        calculateFreeCashFlowOutOfMarketCap: function (freeCashFlow, marketCap) {
-            return (freeCashFlow / marketCap)
-        },
-
-        calculateEarnigsAveregOutOfFreeCashFlow: function (freeCashFlow, response) {
-            const earnings = response.earnings;
-            if (earnings) {
-                const calc = earnings.financialsChart.quarterly.reduce((prev, q) => {
-                    return ({
-                        avarage: ((prev.avarage + q.earnings.raw) / (prev.total + 1)),
-                        total: prev.total + 1,
-                        accum: prev.avarage + q.earnings.raw
-                    })
-                }, { avarage: 0, total: 0 })
-                return (calc.avarage / freeCashFlow)
-
-            }
-
-
-
-            const earningsAvg = earnings
-        },
 
 
 
 
         buildSymbolStats: function (symbolData) {
-            const PE = this.extractPE(symbolData);
 
-            const earningsCalc = extractEarnings(symbolData);
+            const foo = {
+                extractPE: function (response) {
+                    if (response.summaryDetail.trailingPE)
+                        return response.summaryDetail.trailingPE.raw
+                    else return "cannot find"
+                },
 
-            const marketCap = extractMarketCap(symbolData);
 
-            const freeCashFlow = extractFreeCashFlow(symbolData);
 
-            const price = extractCurrentPrice(symbolData);
+                extractMarketCap: function (response, formatted = true) {
+                    if (response.summaryDetail.marketCap)
+                        if (formatted)
+                            return response.summaryDetail.marketCap.fmt
+                        else return response.summaryDetail.marketCap.raw
+                    else return "cannot find"
+                },
 
-            const earningsReduced = reduceEarningObject(earningsCalc)
 
-            const FCS_CAP_value = calculateFreeCashFlowOutOfMarketCap(extractFreeCashFlow(symbolData, false), extractMarketCap(symbolData, false));
+                extractCurrentPrice: function (response) {
+                    if (response.price.regularMarketPrice)
+                        return response.price.regularMarketPrice.raw
+                    else return "cannot find"
+                },
 
-            const FCS_earningsAvg_value = calculateEarnigsAveregOutOfFreeCashFlow(extractFreeCashFlow(symbolData, false), symbolData);
+
+                extractFreeCashFlow: function (response, formatted = true) {
+                    if (response.timeSeries)
+                        if (formatted)
+                            return response.timeSeries.trailingFreeCashFlow[0].reportedValue.fmt
+                        else
+                            return response.timeSeries.trailingFreeCashFlow[0].reportedValue.raw
+                    else return "cannot find"
+                },
+
+
+                extractEarnings: function (responseS) {
+                    if (responseS === undefined) return
+
+                    const earnings = responseS.earnings;
+                    if (earnings) {
+                        const calc = earnings.financialsChart.quarterly.reduce((prev, q) => {
+                            return ({
+                                ...prev,
+                                [q.date]: {
+                                    date: q.date,
+                                    earnings: q.earnings.fmt.toString(),
+                                    revenue: q.revenue.fmt.toString(),
+                                    calc: ((q.earnings.raw / q.revenue.raw))
+                                }
+                            })
+                        }, {})
+
+                        return calc
+                    }
+                    else return {}
+
+
+                },
+                reduceEarningObject: function (earnings) {
+
+                    const earningsKeys = Object.keys(earnings);
+
+                    return earningsKeys.reduce((prev, current) => {
+
+                        const Q_data = earnings[current];
+                        prev.push({ key: "revenue_" + Q_data.date, value: Q_data.revenue })
+                        prev.push({ key: "earnings_" + Q_data.date, value: Q_data.earnings })
+                        prev.push({ key: "calc_" + Q_data.date, value: Q_data.calc })
+                        return prev
+                    }, [])
+                },
+
+                calculateFreeCashFlowOutOfMarketCap: function (freeCashFlow, marketCap) {
+                    return (freeCashFlow / marketCap)
+                },
+
+                calculateEarnigsAveregOutOfFreeCashFlow: function (freeCashFlow, response) {
+                    const earnings = response.earnings;
+                    if (earnings) {
+                        const calc = earnings.financialsChart.quarterly.reduce((prev, q) => {
+                            return ({
+                                avarage: ((prev.avarage + q.earnings.raw) / (prev.total + 1)),
+                                total: prev.total + 1,
+                                accum: prev.avarage + q.earnings.raw
+                            })
+                        }, { avarage: 0, total: 0 })
+                        return (calc.avarage / freeCashFlow)
+
+                    }
+
+
+
+                    const earningsAvg = earnings
+                },
+
+
+            }
+
+
+            const PE = foo.extractPE(symbolData);
+
+            const earningsCalc = foo.extractEarnings(symbolData);
+
+            const marketCap = foo.extractMarketCap(symbolData);
+
+            const freeCashFlow = foo.extractFreeCashFlow(symbolData);
+
+            const price = foo.extractCurrentPrice(symbolData);
+
+            const earningsReduced = foo.reduceEarningObject(earningsCalc)
+
+            const FCS_CAP_value = foo.calculateFreeCashFlowOutOfMarketCap(extractFreeCashFlow(symbolData, false), extractMarketCap(symbolData, false));
+
+            const FCS_earningsAvg_value = foo.calculateEarnigsAveregOutOfFreeCashFlow(extractFreeCashFlow(symbolData, false), symbolData);
 
 
             return ([
