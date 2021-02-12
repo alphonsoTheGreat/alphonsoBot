366a9b1fea10341030c33831b1c47df892fe2aa3
itayt@foresightauto.com <itayt@foresightauto.com>
2021-02-12 15:05:41 +0200

    fox

--- utils/logger/index.js
+++ utils/logger/index.js
@@ -7,6 +7,6 @@ module.exports = {
     ERROR: (placeHolder, error) => {
 
         // const date = new Date()
-        console.log("[ERROR][" + placeHolder + "][" + error.message + "]" + e.stack)
+        console.log("[ERROR][" + placeHolder + "][" + error.message + "]" + error.stack)
     },
 }
